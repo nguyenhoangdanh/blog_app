8c8f3ca66f4192197bb0470cf82e98c4c54f756e
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-01-23 09:14:57 +0700

    create and run server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,20 @@ import { About } from './pages/About'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Dashboard } from './pages/Dashboard'
+import { Header } from './components/Header'
+import './index.css'
+import { Projects } from './pages/Projects'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route  />
     </Routes>
