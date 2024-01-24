6a3c03218789d897632c2aba687ad44a2002ecbc
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-01-24 23:20:20 +0700

    test API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+import userRoutes from './routes/user.route.js'
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 ')
 })
+
+
+app.use('/api/test', userRoutes)
+
