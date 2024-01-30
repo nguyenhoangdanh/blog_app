7a4dcd69e2201cbcd4c6e6518e3bc7a199c51bd6
nguyenhoangdanh.io <hoangdanh54317@gmail.com>
2024-01-30 13:12:33 +0700

    create sign-in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { signin, signup } from '../controller/auth.controller.js';
 
 const router = express.Router();
 
 
-router.post("/sign-up", signup)
+router.post("/sign-up", signup);
+router.post("/sign-in", signin)
+
 
 export default router;
 
